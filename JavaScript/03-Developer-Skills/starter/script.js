9f8b421d2bf3c6b7617bc26f218b15fd92d409c3
Hugo Faria <altran-hfaria@iefp.pt>
2024-07-29 12:31:28 +0100

    complete section 3

--- JavaScript/03-Developer-Skills/starter/script.js
+++ JavaScript/03-Developer-Skills/starter/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+let temps = [17, 21, 23];
+let temps2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+    let forecast = "... ";
+
+    for(let i = 0; i < arr.length; i++)  {
+        forecast += `${arr[i]}ºC in ${i+1} days ... `; 
+    }
+
+    return forecast;
+}
+
+console.log(printForecast(temps));
+console.log(printForecast(temps2));
